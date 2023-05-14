0cab538e325ba02ceddafe5c7d60ae15ea5d4ecf
sergi <sergioserra.2003@gmail.com>
2023-05-14 22:05:04 +0100

    Simulator Card Payment

--- src/main/java/com/luxrest/gui/Controllers/Dashboard/Modules/Payments/PaymentHeaderController.java
+++ src/main/java/com/luxrest/gui/Controllers/Dashboard/Modules/Payments/PaymentHeaderController.java
@@ -1,22 +1,33 @@
 package com.luxrest.gui.Controllers.Dashboard.Modules.Payments;
 
 import com.luxrest.gui.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
 public class PaymentHeaderController {
+    @FXML
     public VBox body;
+    @FXML
     public Button cashButton;
+    @FXML
+    public Button cardButton;
+    @FXML
+    public HBox paymentMethods;
 
     public void initialize() {
         showContentCash();
     }
 
     public void showContentCash() {
+        paymentMethods.getChildren().forEach(node -> node.getStyleClass().remove("selectedOptionPaymentButton"));
+        cashButton.getStyleClass().add("selectedOptionPaymentButton");
         body.getChildren().clear();
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Dashboard/Modules/Payments/PaymentCashBody.fxml"));
         try {
@@ -28,6 +39,8 @@ public class PaymentHeaderController {
     }
 
     public void showContentCard() {
+        paymentMethods.getChildren().forEach(node -> node.getStyleClass().remove("selectedOptionPaymentButton"));
+        cardButton.getStyleClass().add("selectedOptionPaymentButton");
         body.getChildren().clear();
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Dashboard/Modules/Payments/PaymentCardBody.fxml"));
         try {
